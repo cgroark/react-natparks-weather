5053aaed2bca73584e94ce99eb16cf205975286b
cgroark <colingroark@gmail.com>
2018-04-25 16:57:10 -0700

    reaching weather api

--- src/Components/Weather.js
+++ src/Components/Weather.js
@@ -1,8 +1,26 @@
 import React, {Component} from 'react';
+let weatherApi = '4777b8d76e2b5feb';
 
 class Weather extends Component {
-	
+	constructor(props){
+		super(props)
+		this.state = {
+			location: ''
+		}
+		let latlong = this.props.latLong;
+		let split = latlong.split(':').slice(1,3).join().split(',');
+  		let gone = split.splice(1,1);
+  		let weatherUrl = 'http://api.wunderground.com/api/' + weatherApi + '/forecast10day/q/' +  split.join() + '.json';
+  		fetch(weatherUrl)
+  			.then(response => {
+  				return response.json()
+  			}).then(json => {
+  				console.log(json)
+  			})
+	}
+
 	render(){
+		
 		return(
 			<div>
 				{this.props.latLong}
